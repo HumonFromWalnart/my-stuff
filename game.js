242f61340283eb9c5d49292cf603f6e951176880
HumonFromWalnart <tlmuunulzii@gmail.com>
2022-09-06 23:20:06 +0800

    rock,paper,scissors finished

--- game.js
+++ game.js
@@ -15,77 +15,80 @@ function randomNumber() {
   }
   return result;
 }
-function game(){
+// function game(){
+//   const bot = randomNumber();
+//   const player = "rock";
+//   if(bot === "scissors" && player === "rock"){
+//     playerScore++;
+//     document.getElementsByClassName("number")[0].innerHTML = playerScore;
+//     document.getElementsByClassName("status")[0].innerHTML  = "You win";
+//   }
+//   if(bot == "rock" && player === "rock"){
+//     document.getElementsByClassName("status")[0].innerHTML  = "Draw";
+//   }
+//   if(bot == "paper" && player === "rock"){
+//     botScore++;
+//     document.getElementsByClassName("number")[1].innerHTML = botScore;
+//     document.getElementsByClassName("status")[0].innerHTML = "You lose";
+//   }
+// }
+function scissors() {
+  playerValue = "scissors";
   const bot = randomNumber();
-  const player = "rock";
-  if(bot === "scissors" && player === "rock"){
-    playerScore++;
-    document.getElementsByClassName("number")[0].innerHTML = playerScore;
-    document.getElementsByClassName("status")[0].innerHTML  = "You win";
+  if (bot == "scissors" && playerValue === "scissors") {
+    document.getElementsByClassName("status")[0].innerHTML = "Draw";
+    console.log(`${playerValue} vs ${bot}`);
   }
-  if(bot == "rock" && player === "rock"){
-    document.getElementsByClassName("status")[0].innerHTML  = "Draw";
-  }
-  if(bot == "paper" && player === "rock"){
+  if (bot == "rock" && playerValue === "scissors") {
     botScore++;
     document.getElementsByClassName("number")[1].innerHTML = botScore;
     document.getElementsByClassName("status")[0].innerHTML = "You lose";
+    console.log(`${playerValue} vs ${bot}`);
+  }
+  if (bot == "paper" && playerValue === "scissors") {
+    playerScore++;
+    document.getElementsByClassName("number")[0].innerHTML = playerScore;
+    document.getElementsByClassName("status")[0].innerHTML = "You win";
+    console.log(`${playerValue} vs ${bot}`);
   }
 }
-// function scissors() {
-//   playerValue = "scissors";
-//   if (bot === "scissors" && playerValue === "scissors") {
-//     document.getElementsByClassName("status")[0].innerHTML = "Draw";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-//   if (bot === "rock" && playerValue === "scissors") {
-//     botScore++;
-//     document.getElementsByClassName("number")[1].innerHTML = botScore;
-//     document.getElementsByClassName("status")[0].innerHTML = "You lose";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-//   if (bot === "paper" && playerValue === "scissors") {
-//     playerScore++;
-//     document.getElementsByClassName("number")[0].innerHTML = playerScore;
-//     document.getElementsByClassName("status")[0].innerHTML = "You win";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-// }
-// function rock() {
-//   playerValue = "rock";
-//   if (bot === "scissors" && playerValue === "rock") {
-//     playerScore++;
-//     document.getElementsByClassName("number")[0].innerHTML = playerScore;
-//     document.getElementsByClassName("status")[0].innerHTML = "You win";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-//   if (bot === "rock" && playerValue === "rock") {
-//     document.getElementsByClassName("status")[0].innerHTML = "Draw";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-//   if (bot === "paper" && playerValue === "rock") {
-//     botScore++;
-//     document.getElementsByClassName("number")[1].innerHTML = botScore;
-//     document.getElementsByClassName("status")[0].innerHTML = "You lose";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-// }
-// function paper() {
-//   playerValue = "paper";
-//   if (bot === "scissors" && playerValue === "paper") {
-//     botScore++;
-//     document.getElementsByClassName("number")[1].innerHTML = botScore;
-//     document.getElementsByClassName("status")[0].innerHTML = "You lose";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-//   if (bot === "rock" && playerValue === "paper") {
-//     playerScore++;
-//     document.getElementsByClassName("number")[0].innerHTML = playerScore;
-//     document.getElementsByClassName("status")[0].innerHTML = "You win";
+function rock() {
+  playerValue = "rock";
+  const bot = randomNumber();
+  if (bot == "scissors" && playerValue === "rock") {
+    playerScore++;
+    document.getElementsByClassName("number")[0].innerHTML = playerScore;
+    document.getElementsByClassName("status")[0].innerHTML = "You win";
+    console.log(`${playerValue} vs ${bot}`);
+  }
+  if (bot == "rock" && playerValue === "rock") {
+    document.getElementsByClassName("status")[0].innerHTML = "Draw";
+    console.log(`${playerValue} vs ${bot}`);
+  }
+  if (bot == "paper" && playerValue === "rock") {
+    botScore++;
+    document.getElementsByClassName("number")[1].innerHTML = botScore;
+    document.getElementsByClassName("status")[0].innerHTML = "You lose";
+    console.log(`${playerValue} vs ${bot}`);
+  }
+}
+function paper() {
+  playerValue = "paper";
+  const bot = randomNumber();
+  if (bot == "scissors" && playerValue === "paper") {
+    botScore++;
+    document.getElementsByClassName("number")[1].innerHTML = botScore;
+    document.getElementsByClassName("status")[0].innerHTML = "You lose";
+    console.log(`${playerValue} vs ${bot}`);
+  }
+  if (bot == "rock" && playerValue === "paper") {
+    playerScore++;
+    document.getElementsByClassName("number")[0].innerHTML = playerScore;
+    document.getElementsByClassName("status")[0].innerHTML = "You win";
 
-//   }
-//   if (bot === "paper" && playerValue === "paper") {
-//     document.getElementsByClassName("status")[0].innerHTML = "Draw";
-//     console.log(`${playerValue} vs ${bot}`);
-//   }
-// }
+  }
+  if (bot == "paper" && playerValue === "paper") {
+    document.getElementsByClassName("status")[0].innerHTML = "Draw";
+    console.log(`${playerValue} vs ${bot}`);
+  }
+}
